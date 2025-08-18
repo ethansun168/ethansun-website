6b69496d3561e6db36dc17d47b43a41c688e2d25
ethansun <ethansun@umich.edu>
2025-08-18 16:22:03 -0400

    backend hono client and type validation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,42 @@
-import dotenv from 'dotenv';
 import { serve } from '@hono/node-server';
+import dotenv from 'dotenv';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { verifyPassword } from './auth.js';
 import { getUser } from './db.js';
+import { zValidator } from '@hono/zod-validator';
+import z from 'zod';
 
 dotenv.config();
 const app = new Hono()
 
-app.use("*", cors());
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const route = app.use("*", cors())
+.get('/', (c) => {
+    return c.text('Hello Hono!')
 })
-
-app.get('/api/v1', (c) => {
+.get('/api/v1', (c) => {
     return c.json({"hello": "world"})
 })
-
-app.post('/api/v1/login', async (c) => {
-    const body = await c.req.json();
-    const username = body.username;
-    const password = body.password;
-    const user = await getUser(username);
-    console.log(user);
-    if (!user) {
-        return c.json({"message": "Failed to get user"}, 400);
-    }
-    if (!await verifyPassword(password, user.password)) {
-        return c.json({"message": "Wrong password"}, 401)
-    }
-
-    return c.json(user)
-})
+.post(
+    '/api/v1/login',
+    zValidator(
+        'json',
+        z.object({
+            username: z.string(),
+            password: z.string(),
+        })
+    ), async (c) => {
+        const {username, password} = c.req.valid('json');
+        const user = await getUser(username);
+        console.log(user);
+        if (!user) {
+            return c.json({"message": "Failed to get user"}, 400);
+        }
+        if (!await verifyPassword(password, user.password)) {
+            return c.json({"message": "Wrong password"}, 401)
+        }
+        return c.json(user)
+    })
 
 serve({
   fetch: app.fetch,
@@ -40,3 +44,5 @@ serve({
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
+
+export type AppType = typeof route;
