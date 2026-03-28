e38fdfcb4d03d6c6257fa3a37e7f65ac1ac0cc48
ethansun <ethansun@umich.edu>
2026-03-27 21:44:52 -0400

    backend url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,8 +1,8 @@
 import type { AppType, MinecraftApp } from 'ethansun-website-backend';
 import { hc } from 'hono/client';
 
-// export const BACKEND_URL = "https://api.ethansun.org";
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "https://api.ethansun.org";
+// export const BACKEND_URL = "http://localhost:3000";
 export const minecraftClient = hc<MinecraftApp>(BACKEND_URL,
   {
     init: {
